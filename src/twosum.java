ea0f6f8d0d467ef96fac9e6089b3d23de6f030d8
Fathima rasha <rashawin25@gmail.com>
2026-03-05 19:29:06 +0530

    revision 1.twosum

--- src/twosum.java
+++ src/twosum.java
@@ -29,7 +29,7 @@ public class twosum{
         for(int i=0;i<arr.length;i++){
             if(!map.containsKey(target-map.get(arr[i]))){
                 map.put(arr[i],i);
-
+//
             }
             else{
                 index[0]=i;
